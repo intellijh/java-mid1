ab2f2043377b420715965190c6cd51fe3decc476
kjh <kjh4940@naver.com>
2024-03-28 21:29:03 +0900

    no

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import lang.LangMain;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/lang/object/tostring/ToStringMain2.java
@@ -0,0 +1,9 @@
+package lang.object.tostring;
+
+public class ToStringMain2 {
+
+    public static void main(String[] args) {
+        Car car = new Car("Model Y");
+        Dog dog1 = new Dog("멍멍이1", 2);
+    }
+}
