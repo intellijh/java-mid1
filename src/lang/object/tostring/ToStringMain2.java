027de1520ee8e20cf696f2dbe2e33b8df6eb11d2
kjh <kjh4940@naver.com>
2024-03-29 19:38:33 +0900

    no

--- src/lang/object/tostring/ToStringMain2.java
+++ src/lang/object/tostring/ToStringMain2.java
@@ -5,5 +5,7 @@ public class ToStringMain2 {
     public static void main(String[] args) {
         Car car = new Car("Model Y");
         Dog dog1 = new Dog("멍멍이1", 2);
+        Dog dog2 = new Dog("멍멍이2", 5);
+
     }
 }
