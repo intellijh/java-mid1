f3c6964670d1e1899a76fd846534ed987145768c
kjh <kjh4940@naver.com>
2024-04-02 22:52:08 +0900

    no

--- src/lang/immutable/address/MemberMainV2.java
+++ src/lang/immutable/address/MemberMainV2.java
@@ -3,7 +3,7 @@ package lang.immutable.address;
 public class MemberMainV2 {
 
     public static void main(String[] args) {
-        ImmnutableAddress address = new Address("서울");
+        ImmnutableAddress address = new ImmnutableAddress("서울");
 
         MemberV2 memberA = new MemberV2("회원A", address);
         MemberV2 memberB = new MemberV2("회원B", address);
@@ -11,7 +11,7 @@ public class MemberMainV2 {
         System.out.println("memberA = " + memberA);
         System.out.println("memberB = " + memberB);
 
-        memberB.getAddress().setValue("부산");
+        memberB.setAddress(new ImmnutableAddress("부산"));
         System.out.println("부산 -> memberB.address");
         System.out.println("memberA = " + memberA);
         System.out.println("memberB = " + memberB);
